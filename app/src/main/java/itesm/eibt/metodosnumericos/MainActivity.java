318440034a448180630e3cf3389088cc031581be
EthanIsaac <SevenLives565*>
2017-05-11 01:12:23 -0500

    Se muere la matriz inversa. :'v

--- app/src/main/java/itesm/eibt/metodosnumericos/MainActivity.java
+++ app/src/main/java/itesm/eibt/metodosnumericos/MainActivity.java
@@ -20,7 +20,10 @@ import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
+import java.util.Objects;
 
 //RECURSOS
 /* Crear la tabla de datos
@@ -39,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
     private boolean isMenuUp;
     private Button btnGauss;
     private Button btnDivisionSinteticaDoble;
-    private Button btnMinimosCuadrados;
+    private Button btnMinimosInterpolacion;
     private Button btnMenu;
     private Button btnGenerar;
     private Button btnResuelve;
@@ -79,21 +82,232 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) { cargaDivisionSinteticaDoble();
             }
         });
-        btnMinimosCuadrados = (Button) findViewById(R.id.boton_minimoscuadrados);
-        btnMinimosCuadrados.setOnClickListener(new View.OnClickListener() {
+        btnMinimosInterpolacion = (Button) findViewById(R.id.boton_interpolacion);
+        btnMinimosInterpolacion.setOnClickListener(new View.OnClickListener() {
 
             @Override
-            public void onClick(View view) { cargaMinimosCuadrados(); }
+            public void onClick(View view) { cargaInterpolacion(); }
         });
     }
 
-    // MÉTODOS MÍNIMOS CUADRADOS
+    // MÉTODOS INTERPOLACIÓN
 
-    private void cargaMinimosCuadrados()
-    {
-        setContentView(R.layout.activity_divisionsinteticadoble);
+    private void cargaInterpolacion()  {
+        setContentView(R.layout.activity_interpolacion);
         // TODO DESPUÉS DE AQUÍ
         isMenuUp = false;
+        limpiarScroll();
+        crearTablaInterpolacion(3);
+        btnMenu = (Button) findViewById(R.id.boton_menu);
+        btnMenu.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                cargaMenu();
+            }
+        });
+        btnResuelve = (Button) findViewById(R.id.boton_resuelve);
+        btnResuelve.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                ArrayList datos = obtenerDatosInterpolacion((TableLayout)findViewById(R.id.table));
+                if(datos!=null)
+                {
+                    Float[][] m = new Float[3][3];
+                    m[0][0] = 1.0f;
+                    m[0][1] = 1.0f;
+                    m[0][2] = 0.0f;
+                    m[1][0] = 1.0f;
+                    m[1][1] = 0.0f;
+                    m[1][2] = 1.0f;
+                    m[2][0] = 0.0f;
+                    m[2][1] = 1.0f;
+                    m[2][2] = 0.0f;
+                    calcularPolinomioInterpolacion(datos);
+                    calculaInversa(m);
+                }
+            }
+        });
+        btnGenerar = (Button) findViewById(R.id.boton_generar);
+        btnGenerar.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                EditText size = (EditText)findViewById(R.id.edit_size);
+                int sizeNum = 0;
+                try{
+                    sizeNum = Integer.parseInt(size.getText().toString());
+                }catch (Exception e) {  }
+                if(sizeNum!=0)
+                {
+                    limpiarScroll();
+                    crearTablaInterpolacion(sizeNum);
+                }
+                else
+                {
+                    muestraAlerta("¡INGRESA UNA CANTIDAD DE DATOS VÁLIDA (MAYOR A CERO)!");
+                }
+            }
+        });
+    }
+
+    private void calcularPolinomioInterpolacion(ArrayList datos) {
+
+    }
+
+    private Float[] calculaCoeficientes(Float[][] matrizX, Float[] matrizY)   {
+        Float[] coeficientes = null;
+        return coeficientes;
+    }
+
+    private Float[][] calculaInversa(Float [][] matriz)   {
+        int n=matriz.length;  //dimensión de la matriz
+        Float[][] a = matriz;
+        Float[][] b = new Float[n][n];   //matriz de los términos independientes
+        Float[][] c = new Float[n][n];   //matriz de las incógnitas
+//matriz unidad
+        for(int i=0; i<n; i++){
+            b[i][i]=1.0f;
+        }
+//transformación de la matriz y de los términos independientes
+        for(int k=0; k<n-1; k++){
+            for(int i=k+1; i<n; i++){
+//términos independientes
+                for(int s=0; s<n; s++){
+                    b[i][s]-=a[i][k]*b[k][s]/a[k][k];
+                }
+//elementos de la matriz
+                for(int j=k+1; j<n; j++){
+                    a[i][j]-=a[i][k]*a[k][j]/a[k][k];
+                }
+            }
+        }
+//cálculo de las incógnitas, elementos de la matriz inversa
+        for(int s=0; s<n; s++){
+            c[n-1][s]=b[n-1][s]/a[n-1][n-1];
+            for(int i=n-2; i>=0; i--){
+                c[i][s]=b[i][s]/a[i][i];
+                for(int k=n-1; k>i; k--){
+                    c[i][s]-=a[i][k]*c[k][s]/a[i][i];
+                }
+            }
+        }
+        LinearLayout scroll = (LinearLayout) findViewById(R.id.scroll);
+        scroll.addView(imprimeMatrizCuadrada(c));
+        return c;
+    }
+
+    private ArrayList obtenerDatosInterpolacion(TableLayout tabla) {
+        ArrayList datos = new ArrayList();
+        int filas = tabla.getChildCount();
+        for(int i = 1; i<filas;i++)
+        {
+            TableRow dato = (TableRow)tabla.getChildAt(i);
+            String x = ((EditText)dato.getChildAt(0)).getText().toString();
+            String y = ((EditText)dato.getChildAt(1)).getText().toString();
+            if(x.equals("")||y.equals(""))
+            {
+                muestraAlerta("¡NO DEJES CELDAS EN BLANCO!");
+                return null;
+            }
+            datos.add(new parDeDatos(Float.parseFloat(x),Float.parseFloat(y)));
+        }
+        return datos;
+    }
+
+    private void crearTablaInterpolacion(int size){
+        TableLayout tabla = (TableLayout) findViewById(R.id.table);;
+        tabla.removeAllViews();
+        TableRow titulos = new TableRow(this);
+        titulos.setGravity(Gravity.CENTER_HORIZONTAL);
+        TextView xTitulos = new TextView(this);
+        xTitulos.setText("X");
+        xTitulos.setWidth(150);
+        xTitulos.setInputType(InputType.TYPE_CLASS_NUMBER|InputType.TYPE_NUMBER_FLAG_SIGNED|InputType.TYPE_NUMBER_FLAG_DECIMAL);
+        xTitulos.setTextSize(15);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            xTitulos.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+        }
+        xTitulos.setBackgroundColor(Color.GRAY);
+        titulos.addView(xTitulos);
+        TextView yTitulos = new TextView(this);
+        yTitulos.setText("Y");
+        yTitulos.setWidth(150);
+        yTitulos.setInputType(InputType.TYPE_CLASS_NUMBER|InputType.TYPE_NUMBER_FLAG_SIGNED|InputType.TYPE_NUMBER_FLAG_DECIMAL);
+        yTitulos.setTextSize(15);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            yTitulos.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+        }
+        yTitulos.setBackgroundColor(Color.GRAY);
+        titulos.addView(yTitulos);
+        tabla.addView(titulos);
+        for(int i=0;i<size;i++)
+        {
+            TableRow tr = new TableRow(this);
+            tr.setGravity(Gravity.CENTER_HORIZONTAL);
+            EditText x = new EditText(this);
+            x.setWidth(150);
+            x.setInputType(InputType.TYPE_CLASS_NUMBER|InputType.TYPE_NUMBER_FLAG_SIGNED|InputType.TYPE_NUMBER_FLAG_DECIMAL);
+            x.setTextSize(15);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+                x.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+            }
+            tr.addView(x);
+            EditText y = new EditText(this);
+            y.setWidth(150);
+            y.setInputType(InputType.TYPE_CLASS_NUMBER|InputType.TYPE_NUMBER_FLAG_SIGNED|InputType.TYPE_NUMBER_FLAG_DECIMAL);
+            y.setTextSize(15);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+                y.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+            }
+            tr.addView(y);
+            tabla.addView(tr);
+        }
+    }
+
+    private class parDeDatos extends Object{
+        private Float x;
+        private Float y;
+        public parDeDatos(Float x, Float y){
+            this.x = x;
+            this.y = y;
+        }
+        public void setX(Float x){
+            this.x = x;
+        }
+        public void setY(Float y){
+            this.y = y;
+        }
+        public Float getX(){
+            return this.x;
+        }
+        public Float getY(){
+            return this.y;
+        }
+    }
+
+    private TableLayout imprimeMatrizCuadrada(Float[][] matriz)  {
+        int size = matriz.length;
+        TableLayout tabla = new TableLayout(this);
+        for(int i=0;i<size;i++)
+        {
+            TableRow tr = new TableRow(this);
+            tr.setGravity(Gravity.CENTER_HORIZONTAL);
+            for(int j=0;j<size;j++)
+            {
+                TextView tv = new TextView(this);
+                tv.setWidth(150);
+                tv.setTextSize(15);
+                tv.setText(""+matriz[i][j]);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+                    tv.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+                }
+                tr.addView(tv);
+            }
+            tabla.addView(tr);
+        }
+        return tabla;
     }
 
     // MÉTODOS DIVISIÓN SINTÉTICA
@@ -525,7 +739,6 @@ public class MainActivity extends AppCompatActivity {
             tabla.addView(tr);
         }
         return tabla;
-
     }
 
     private Float[][] ordenarMatriz(Float[][] matriz) {
